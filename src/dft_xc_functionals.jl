82a06117a6fa91e1b23bacb81746b2f521873196
Sergei Malykhin <s.e.malykhin@gmail.com>
2020-12-06 20:15:12 +0700

    docstrings added to DFT functionals

--- src/dft_xc_functionals.jl
+++ src/dft_xc_functionals.jl
@@ -1,15 +1,31 @@
-# P. A. M. Dirac, Math. Proc. Cambridge Philos. Soc. 26, 376 (1930) (doi: 10.1017/S0305004100016108)
-# F. Bloch, Z. Phys. 57, 545 (1929) (doi: 10.1007/BF01340281)
-# J.C. Slater, Phys. Rev. 81, 385 (1951)
-# http://doi.org/10.1103/PhysRev.81.385
+"""
+    LDA_X(ρ; α = 2/3)
+
+Gives Slater's approximation of the exchange energy density 
+``\\epsilon_x``, and potential ``V_x``.
+    
+## References
+
+1. P. A. M. Dirac, Math. Proc. Cambridge Philos. Soc. 26, 376 (1930) (doi: 10.1017/S0305004100016108)
+2. F. Bloch, Z. Phys. 57, 545 (1929) (doi: 10.1007/BF01340281)
+3. J.C. Slater, Phys. Rev. 81, 385 (1951) (doi: 10.1103/PhysRev.81.385)
+"""
 function LDA_X(ρ; α = 2/3)
   vx = -3α * (3ρ/8π)^(1/3)
   ϵx = 3/4 * vx
   return vx, ϵx
 end
 
-# T. Chachiyo, J. Chem. Phys. 145, 021101 (2016);
-# https://doi.org/10.1063/1.4958669
+"""
+    LDA_C_CHACHIYO(ρ; a  = -0.01554535, b₁ = 20.4562557, b  = 20.4562557)
+
+Gives Chachiyo's approximation of the correlation energy density 
+``\\epsilon_x``, and potential ``V_x``.
+    
+## References
+
+1. T. Chachiyo, J. Chem. Phys. 145, 021101 (2016) (doi: 10.1063/1.4958669)
+"""
 function LDA_C_CHACHIYO(ρ; a  = -0.01554535, b₁ = 20.4562557, b  = 20.4562557)
 
   ρ <= eps() && return 0.0, 0.0
@@ -21,17 +37,19 @@ function LDA_C_CHACHIYO(ρ; a  = -0.01554535, b₁ = 20.4562557, b  = 20.4562557
   return vc, ϵc
 end
 
-# V. V. Karasiev, J. Chem. Phys. 145, 157101 (2016) (doi: 10.1063/1.4964758)
-# LDA_C_KARASIEV_MOD(ρ) = LDA_C_CHACHIYO(ρ, b₁ = 21.7392245)
+"""
+    LDA_C_VWN(ρ; a = 0.0310907, b = 3.72744, c = 12.9352, x0 = -0.10498)
 
-# S.H. Vosko, L. Wilk, and M. Nusair, Can. J. Phys. 58, 1200 (1980)
-# http://doi.org/10.1139/p80-159
-function LDA_C_VWN(ρ)
+Gives VWN5 approximation of the correlation energy density 
+``\\epsilon_x``, and potential ``V_x``.
+    
+## References
+
+1. S.H. Vosko, L. Wilk, and M. Nusair, Can. J. Phys. 58, 1200 (1980) (doi: 10.1139/p80-159)
+"""
+function LDA_C_VWN(ρ; a = 0.0310907, b = 3.72744, c = 12.9352, x0 = -0.10498)
   ρ <= eps() && return 0.0, 0.0
-
   rs = (3/4π/ρ)^(1/3)
-
-  a = 0.0310907; b = 3.72744; c = 12.9352; x0 = -0.10498;
   q = sqrt(4.0 * c - b * b)
   f1 = 2.0 * b / q
   f2 = b * x0 / (x0 * x0 + b * x0 + c)
@@ -45,3 +63,7 @@ function LDA_C_VWN(ρ)
   vc = ϵc - rs12 * a / 6.0 * (2.0 / rs12 - tx / fx - 4.0 * b / tt - f2 * (2.0 / (rs12 - x0) - tx / fx - 4.0 * (2.0 * x0 + b) / tt))
   return vc, ϵc
 end
+
+# TODO:
+# V. V. Karasiev, J. Chem. Phys. 145, 157101 (2016) (doi: 10.1063/1.4964758)
+# LDA_C_KARASIEV_MOD(ρ) = LDA_C_CHACHIYO(ρ, b₁ = 21.7392245)
