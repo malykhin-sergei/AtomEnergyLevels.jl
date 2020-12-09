b1b3ff220b340115cd9951c692a6116f36264cf6
Sergei Malykhin <s.e.malykhin@gmail.com>
2020-12-09 16:04:19 +0700

    Now code can be used with Libxc library

---
+++ examples/libxc-usage.jl
@@ -0,0 +1,24 @@
+using AtomEnergyLevels, Libxc
+
+function SLAPW92!(ρ, vxc, εxc)
+    lda_x = Functional(:lda_x)
+    lda_c = Functional(:lda_c_pw)
+    exch = evaluate(lda_x, rho=ρ)
+    corr = evaluate(lda_c, rho=ρ)
+    for i in eachindex(ρ)
+        vxc[i] = exch.vrho[i] + corr.vrho[i]
+        εxc[i] = exch.zk[i] + corr.zk[i]
+    end
+end
+
+function atom_ground_state(z, xcfun)
+    ψ = lda(z, xc! = xcfun, β = 0.5).orbitals;
+    for (quantum_numbers, orbital) in sort(collect(ψ), by = x -> last(x).ϵᵢ)
+      nᵣ, l = quantum_numbers
+      nᵢ, ϵᵢ = orbital.nᵢ, orbital.ϵᵢ
+      n = nᵣ + l + 1
+      @printf("\t%i%s\t(%4.1f)\t%14.6f\n", n, atomic_shell[l], nᵢ, ϵᵢ)
+    end
+end
+  
+atom_ground_state(18, SLAPW92!)
