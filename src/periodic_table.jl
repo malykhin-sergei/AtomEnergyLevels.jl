b6753be729948f6f6e0df1ec6dc4599d2d9add3c
Sergei Malykhin <s.e.malykhin@gmail.com>
2021-01-19 20:50:39 +0700

    shells are better as simple string

--- src/periodic_table.jl
+++ src/periodic_table.jl
@@ -1,6 +1,5 @@
 # Sober Physicists Don't Find Giraffes Hiding In Kitchens
-shells = Dict(0 => 'S', 1 => 'P', 2 => 'D', 3 => 'F',
-              4 => 'G', 5 => 'H', 6 => 'I', 7 => 'K')
+shells = "SPDFGHIK"
 
 # Electronic configurations of the elements, taken from
 # https://math.nist.gov/DFTdata/atomdata/configuration.html
